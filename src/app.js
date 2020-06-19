5b8933d9c88134453a01de73cff1deb70ca47daa
nicholas caetano <nichollas36@hotmail.com>
2020-06-19 03:00:30 -0300

    desafio 1 final

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +9,75 @@ app.use(cors());
 
 const repositories = [];
 
+
 app.get("/repositories", (request, response) => {
-  // TODO
+
+    return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+    const project = request.body;
+
+    const id = uuid();
+    project.id = id;
+    project.likes = 0;
+
+    repositories.push(project);
+
+    return response.json(project);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+   const { id }  = request.params;
+   const { title,url,techs } = request.body;
+
+   const idEncontrado = repositories.findIndex(item => item.id === id);
+
+    if(idEncontrado < 0){
+      return response.status('400').json({ "mensagem": "usaurio não encontrado"});
+    }
+
+    const project = {
+      title,
+      url,
+      techs,
+      id,
+      likes: repositories[idEncontrado].likes
+    }
+
+    repositories[idEncontrado] = project;
+
+   return response.json(project);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", async (request, response) => {
+  const { id } = request.params;
+
+  const idEncontrado = repositories.findIndex(item => item.id === id);
+
+  if(idEncontrado < 0){
+    return response.status('400').json({ "mensagem": "usuario não encontrado"});
+  }
+
+  await repositories.splice(idEncontrado,1);
+
+  return response.status('204').json(repositories[idEncontrado]);
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+    const { id } = request.params;
+
+    const idEncontrado = repositories.findIndex(item => item.id === id);
+
+    if(idEncontrado < 0){
+      return response.status('400').json({ "mensagem": "usuario não encontrado"});
+    }
+
+    
+    repositories[idEncontrado].likes++;
+
+    return response.json(repositories[idEncontrado]);
 });
 
 module.exports = app;
